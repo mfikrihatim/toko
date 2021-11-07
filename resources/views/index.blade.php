5cbb2250fa505c5a3b1313c67c2603a3a59f705b
bagusshndr <bagussuhendri19@gmail.com>
2021-11-07 12:10:06 +0700

    PUSH ADD PRODUCT NEW ORDER

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,8 @@
   <link rel='stylesheet' href='https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css'>
   <link rel='stylesheet' href='https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css'>
   <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/boxicons@2.0.0/css/boxicons.min.css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.css'>
   <link rel="stylesheet" href="{{ asset('assets/style.css') }}">
 
 </head>
