187012c961505c5475f4c1e96e96c4e29b23a80f
bagusshndr <bagussuhendri19@gmail.com>
2021-11-07 15:39:06 +0700

    Modal New Order

--- resources/views/new_order/index.blade.php
+++ resources/views/new_order/index.blade.php
@@ -1,11 +1,8 @@
 @extends('index')
 @section('content')
-
-
-
 <section id="content-wrapper">
-    <div class="row py-5">
-      <div class="col-lg-12 py-5">
+    <div class="row pt-0">
+      <div class="col-lg-12 pt-0">
         <h2 class="content-title">Test</h2>
         
         <form action="{{ route('new_order.simpan') }}" method="POST">
@@ -80,17 +77,48 @@
           </button>
         </div>
         <div class="modal-body">
-          <h3>Modal Body</h3>
+          <div class="row">
+            <div class="col-lg-12">
+              <h2 class="content-title">Product Page</h2>
+              <form action="{{ route('product.simpan') }}" method="POST">
+                @csrf
+                  <div class="mb-3">
+                    <div class="form-group">
+                      <label for="sel1">Product ID</label>
+                      <select class="form-control" id="sel1" name="product_id">
+                        <option selected="selected">1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Product Name</label>
+                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="product_name"> 
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Unit Price</label>
+                    <input type="number" class="form-control" id="exampleInputPassword1" name="unit_price">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">QTY</label>
+                    <input type="number" class="form-control" id="exampleInputPassword1" name="qty">
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
           <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
+      <div></div>
     </div>
   </div>
 </section>
-
 <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js'></script>
@@ -99,5 +127,6 @@
 <script src='https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js'>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <script src='https://unpkg.com/popper.js'></script>
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
 </script><script  src="assets/script.js"></script>
 @endsection
